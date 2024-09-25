757a9f754955b9bbb44b9228e696b96c71da8da1
JinSeoHyun <15roa1121@gmail.com>
2024-09-26 04:23:59 +0900

    feat: 임시 게시판 구현

---
+++ boomerang/src/main/java/boomerang/domain/board/domain/Board.java
@@ -0,0 +1,19 @@
+package boomerang.domain.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Builder
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
