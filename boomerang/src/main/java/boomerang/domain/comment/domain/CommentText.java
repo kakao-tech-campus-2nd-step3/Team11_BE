d2b4e04bf45d01b63c96a51d2dee2d192040707f
JinSeoHyun <15roa1121@gmail.com>
2024-09-27 04:40:55 +0900

    fix: @NoArgsConstructor으로 빈 생성자가 없어서 엔티티 만들때 오류 나는 부분 해결

--- boomerang/src/main/java/boomerang/domain/comment/domain/CommentText.java
+++ boomerang/src/main/java/boomerang/domain/comment/domain/CommentText.java
@@ -2,18 +2,23 @@ package boomerang.domain.comment.domain;
 
 import boomerang.global.exception.BusinessException;
 import boomerang.global.response.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Embeddable
 @ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentText {
 
     @Column(name = "comment_text")
     private String value;
 
+    @JsonCreator
     public CommentText(String value) {
         if (value == null || value.trim().isEmpty()) {
             throw new BusinessException(ErrorCode.COMMENT_VALUE_IS_EMPTY);
