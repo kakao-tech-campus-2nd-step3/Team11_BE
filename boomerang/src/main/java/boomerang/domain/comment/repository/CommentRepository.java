fc5e0d571c073f90bcc2c54b56bb0a4f47f216af
JinSeoHyun <15roa1121@gmail.com>
2024-09-27 17:10:27 +0900

    feat: 댓글 페이지 조회시, 가장 최신 댓글을 위로 보이게 함

--- boomerang/src/main/java/boomerang/domain/comment/repository/CommentRepository.java
+++ boomerang/src/main/java/boomerang/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package boomerang.domain.comment.repository;
 
 import boomerang.domain.comment.domain.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    // 보드 ID에 따른 댓글을 createdAt 기준으로 내림차순 정렬
+    Page<Comment> findAllByBoardIdOrderByCreatedAtDesc(Pageable pageable, Long boardId);
 
 }
