d0977095656cb2818382d247a0411817480c448a
JinSeoHyun <15roa1121@gmail.com>
2024-09-27 17:15:27 +0900

    feat: Member에 equals와 hashCode구현

--- boomerang/src/main/java/boomerang/domain/member/domain/Member.java
+++ boomerang/src/main/java/boomerang/domain/member/domain/Member.java
@@ -3,6 +3,8 @@ package boomerang.domain.member.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 @Builder
 @Getter
 @Entity
@@ -16,4 +18,18 @@ public class Member {
 
     private String email;
 
+
+    //email 기준으로 유저값확인
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) && Objects.equals(email, member.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
 }
