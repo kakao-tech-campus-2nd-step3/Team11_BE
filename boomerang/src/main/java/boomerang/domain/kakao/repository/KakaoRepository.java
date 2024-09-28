5f4f3ac42160dbc19edc4298047d8caed258cd3e
Dalsungmin <cnww123@naver.com>
2024-09-28 14:27:56 +0900

    feat: 카카오 레포지토리 Mycrud에서 Jpa로 변경

--- boomerang/src/main/java/boomerang/domain/kakao/repository/KakaoRepository.java
+++ boomerang/src/main/java/boomerang/domain/kakao/repository/KakaoRepository.java
@@ -2,7 +2,8 @@ package boomerang.domain.kakao.repository;
 
 import boomerang.domain.kakao.domain.KakaoDomain;
 import boomerang.global.MyCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KakaoRepository extends MyCrudRepository<KakaoDomain, Long> {
+public interface KakaoRepository extends JpaRepository<KakaoDomain, Long> {
     boolean existsById(Long id);
 }
