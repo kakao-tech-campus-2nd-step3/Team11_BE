4057abad2fdebaff42260777d71a2fd3d4824b60
Dalsungmin <cnww123@naver.com>
2024-09-29 16:59:22 +0900

    refator: Jpa 레포지토리 적용하면서 테이블 생성 방식 변경

--- boomerang/src/main/java/boomerang/domain/member/repository/MemberRepository.java
+++ boomerang/src/main/java/boomerang/domain/member/repository/MemberRepository.java
@@ -1,7 +1,7 @@
 package boomerang.domain.member.repository;
 
 import boomerang.domain.member.domain.MemberDomain;
-import boomerang.global.MyCrudRepository;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<MemberDomain, Long> {
