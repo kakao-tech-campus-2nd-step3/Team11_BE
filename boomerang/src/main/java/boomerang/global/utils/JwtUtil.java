29ed83dc7d1b631ec4493812655d54e7c9b0e32d
JaeBin <201924576@pusan.ac.kr>
2024-10-03 14:12:09 +0900

    feat: token 만료 시간 변수화

--- boomerang/src/main/java/boomerang/global/utils/JwtUtil.java
+++ boomerang/src/main/java/boomerang/global/utils/JwtUtil.java
@@ -11,9 +11,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
-    // application.properties에서 시크릿 키를 가져옵니다.
+    // application.properties에서 시크릿 키와 만료시간을 가져옵니다.
     @Value("${jwt.secret-key}")
     private String secretKey;
+
+    @Value("${jwt.token.expiration-time}")
+    private Long expirationTime;
 
     // Key 객체는 나중에 초기화하기 위해 final이 아닌 필드로 선언합니다.
     private Key key;
@@ -26,29 +29,29 @@ public class JwtUtil {
 
     public String generateToken(Long memberId, String email) {
         return Jwts.builder()
-            .setSubject(memberId.toString())
-            .claim("email", email)
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 2400000L))
-            .signWith(key)
-            .compact();
+                .setSubject(memberId.toString())
+                .claim("email", email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime)) // 설정된 만료 시간 사용
+                .signWith(key)
+                .compact();
     }
 
     public Claims extractClaims(String token) {
         Claims claims = Jwts.parserBuilder()
-            .setSigningKey(key)
-            .build()
-            .parseClaimsJws(token)
-            .getBody();
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
         return claims;
     }
 
     public String getEmail(String token) {
         Claims claims = Jwts.parserBuilder()
-            .setSigningKey(key)
-            .build()
-            .parseClaimsJws(token)
-            .getBody();
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
         return claims.get("email", String.class);
     }
 
