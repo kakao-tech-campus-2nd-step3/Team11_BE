8a5ecbaf957d2a861cb77243d85bcc2efcfa78c4
JaeBin <201924576@pusan.ac.kr>
2024-10-04 19:57:12 +0900

    fix: v1 빠진것 추가

--- boomerang/src/main/java/boomerang/board/controller/BoardController.java
+++ boomerang/src/main/java/boomerang/board/controller/BoardController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/board")
+@RequestMapping("/api/v1/board")
 public class BoardController {
     private final BoardService boardService;
     private final MemberService memberService;
