1f78cf22c71c89d810aecc3a06aa1608bd51044f
JinSeoHyun <15roa1121@gmail.com>
2024-10-07 22:05:31 +0900

    feat: 동일한 /api/v1/board/{board_id}/comments인 경우에도 POST는 권한 요구

--- boomerang/src/main/java/boomerang/global/config/SecurityConfig.java
+++ boomerang/src/main/java/boomerang/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -19,7 +20,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -39,38 +39,38 @@ public class SecurityConfig {
 
         //cors 설정
         http
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
-                @Override
-                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
-                    CorsConfiguration configuration = new CorsConfiguration();
+                        CorsConfiguration configuration = new CorsConfiguration();
 
-                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));        //3000 허용
-                    configuration.setAllowedMethods(Collections.singletonList("*"));                            //모든 HTTP 메서드 허용
-                    configuration.setAllowCredentials(true);                                                    //쿠키 사용
-                    configuration.setAllowedHeaders(Collections.singletonList("*"));                            //클라이언트는 모든 타입의 헤더를 사용
-                    configuration.setMaxAge(3600L);                                                             //프리플라이트 요청 결과 한시간동안 유효
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));        //3000 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));                            //모든 HTTP 메서드 허용
+                        configuration.setAllowCredentials(true);                                                    //쿠키 사용
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));                            //클라이언트는 모든 타입의 헤더를 사용
+                        configuration.setMaxAge(3600L);                                                             //프리플라이트 요청 결과 한시간동안 유효
 
-                    //클라이언트가 응답에서 Set-Cookie, Authorization의 헤더에 접근할 수 있도록 허용
-                    configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                        //클라이언트가 응답에서 Set-Cookie, Authorization의 헤더에 접근할 수 있도록 허용
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-                    return configuration;
-                }
-            }));
+                        return configuration;
+                    }
+                }));
 
         //csrf disable
         http
-            .csrf((auth) -> auth.disable());
+                .csrf((auth) -> auth.disable());
 
         //From 로그인 방식 disable
         http
-            .formLogin((auth) -> auth.disable());
+                .formLogin((auth) -> auth.disable());
 
         //HTTP Basic 인증 방식 disable
         http
-            .httpBasic((auth) -> auth.disable());
+                .httpBasic((auth) -> auth.disable());
 
         // H2 콘솔을 사용하기 위해 프레임 옵션 비활성화
         http
@@ -80,22 +80,23 @@ public class SecurityConfig {
         //JWTFilter 추가 (이후 JWT 필터 구현 후 추가)
         http
                 .addFilterBefore(new JwtFilter(jwtUtil, principalService), UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(handeler-> handeler.authenticationEntryPoint(new SecurityAuthenticationEntryPoint()));
+                .exceptionHandling(handeler -> handeler.authenticationEntryPoint(new SecurityAuthenticationEntryPoint()));
 
         //경로별 인가 작업
         http
-            .authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/api/v1/member","/api/v1/board/comments/**").authenticated()
-                .anyRequest().permitAll());
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/api/v1/member", "/api/v1/board/comments/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/board/*/comments").authenticated() // POST 요청 추가
+                        .anyRequest().permitAll());
 
         //세션 설정 : STATELESS
         http
-            .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         // H2 콘솔을 사용하기 위해 프레임 옵션 비활성화
         http
-            .headers((headersConfigurer) -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+                .headers((headersConfigurer) -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
 
         return http.build();
