d4dcf0697ce24a32a521e68c0d5fac810e48286c
JinSeoHyun <15roa1121@gmail.com>
2024-10-07 21:07:05 +0900

    feat: Comment API 명세서에 맞춰 수정 & @AuthenticationPrincipal 수정

--- boomerang/src/main/java/boomerang/comment/controller/CommentController.java
+++ boomerang/src/main/java/boomerang/comment/controller/CommentController.java
@@ -1,10 +1,10 @@
 package boomerang.comment.controller;
 
-import boomerang.comment.service.CommentService;
 import boomerang.comment.dto.CommentRequestDto;
 import boomerang.comment.dto.CommentResponseDto;
-import boomerang.domain.member.domain.Member;
+import boomerang.comment.service.CommentService;
 import boomerang.global.exception.BusinessException;
+import boomerang.global.oauth.dto.PrincipalDetails;
 import boomerang.global.response.ErrorResponseDto;
 import boomerang.global.utils.ResponseHelper;
 import lombok.RequiredArgsConstructor;
@@ -12,57 +12,49 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class CommentController {
 
     private final CommentService commentService;
 
     //댓글 조회
-    @GetMapping("comment/{board_id}")
-    public ResponseEntity<?> getAllComment(//@AuthenticationPrincipal PrincipalDetails principalDetails,
+    @GetMapping("/board/{board_id}/comments")
+    public ResponseEntity<?> getAllComment(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                            @PathVariable("board_id") Long boardId,
                                            Pageable pageable) {
-        //현재 로그인 기능 미 구현으로 테스트 유저를 집어넣음 (이후 삭제 필요)
-        Member tempMember = Member.builder().email("test123@bomerang.com").build();
-        Page<CommentResponseDto> commentResponsePage = commentService.getAllComment(tempMember.getEmail(), boardId,pageable);
+        Page<CommentResponseDto> commentResponsePage = commentService.getAllComment(principalDetails, boardId, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(commentResponsePage);
     }
 
     //댓글 생성
-    @PostMapping("/comment/{board_id}")
-    public ResponseEntity<?> createComment(//@AuthenticationPrincipal PrincipalDetails principalDetails
-                                            @PathVariable("board_id") Long boardId,
-                                            @RequestBody CommentRequestDto commentRequestDto) {
-        //현재 로그인 기능 미 구현으로 테스트 유저를 집어넣음 (이후 삭제 필요)
-        Member tempMember = Member.builder().email("test123@bomerang.com").build();
-        commentService.createComment(tempMember.getEmail(), boardId, commentRequestDto);
+    @PostMapping("/board/{board_id}/comments")
+    public ResponseEntity<?> createComment(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                           @PathVariable("board_id") Long boardId,
+                                           @RequestBody CommentRequestDto commentRequestDto) {
+        commentService.createComment(principalDetails.getMemberEmail(), boardId, commentRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //댓글 수정
-    @PutMapping("/comment/{board_id}/{comment_id}")
-    public ResponseEntity<?> updateComment(//@AuthenticationPrincipal PrincipalDetails principalDetails
-                                           @PathVariable("board_id") Long boardId,
+    @PutMapping("/board/comments/{comment_id}")
+    public ResponseEntity<?> updateComment(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                            @PathVariable("comment_id") Long commentId,
                                            @RequestBody CommentRequestDto commentRequestDto) {
-        //현재 로그인 기능 미 구현으로 테스트 유저를 집어넣음 (이후 삭제 필요)
-        Member tempMember = Member.builder().email("test123@bomerang.com").build();
-        commentService.updateComment(tempMember.getEmail(), commentId, commentRequestDto);
+        commentService.updateComment(principalDetails.getMemberEmail(), commentId, commentRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //댓글 삭제
-    @DeleteMapping("/comment/{board_id}/{comment_id}")
-    public ResponseEntity<?> deleteComment(//@AuthenticationPrincipal PrincipalDetails principalDetails
-                                           @PathVariable("board_id") Long boardId,
+    @DeleteMapping("/board/comments/{comment_id}")
+    public ResponseEntity<?> deleteComment(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                            @PathVariable("comment_id") Long commentId) {
-        //현재 로그인 기능 미 구현으로 테스트 유저를 집어넣음 (이후 삭제 필요)
-        Member tempMember = Member.builder().email("test123@bomerang.com").build();
-        commentService.deleteComment(tempMember.getEmail(), commentId);
+        commentService.deleteComment(principalDetails.getMemberEmail(), commentId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
