280a26dc189e6a026636a18d9a95fa0e72717fca
Dalsungmin <cnww123@naver.com>
2024-10-11 17:17:48 +0900

    feat: 첫 회원가입시 랜덤 닉네임 생성
    첫 회원가입시 중복되지 않은 랜덤 닉네임 자동 생성

--- boomerang/src/main/java/boomerang/member/service/MemberService.java
+++ boomerang/src/main/java/boomerang/member/service/MemberService.java
@@ -2,6 +2,7 @@ package boomerang.member.service;
 
 import boomerang.kakao.domain.KakaoMember;
 import boomerang.member.domain.Member;
+import boomerang.member.domain.RandonNinkname;
 import boomerang.member.dto.MemberServiceDto;
 import boomerang.member.exception.MemberNotFoundException;
 import boomerang.member.repository.MemberRepository;
@@ -14,10 +15,12 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
+    private final RandonNinkname randonNinkname;
 
-    public MemberService(MemberRepository memberRepository, JwtUtil jwtUtil) {
+    public MemberService(MemberRepository memberRepository, JwtUtil jwtUtil, RandonNinkname randonNinkname) {
         this.memberRepository = memberRepository;
         this.jwtUtil = jwtUtil;
+        this.randonNinkname = randonNinkname;
     }
 
     public List<Member> getAllMembers() {
@@ -41,7 +44,7 @@ public class MemberService {
 
     public String loginKakaoMember(KakaoMember kakaoMember) {
         String email= kakaoMember.email(); //카카오에서 받은 이메일을 emailString으로 저장
-        String nickname = kakaoMember.nickname();
+        String nickname = generateUniqueNickname();// 카카오 닉네임이 아닌 랜덤 닉네임 생성기로부터 닉네임 받기
         System.out.println("email = " + email);
         MemberServiceDto memberCreateServiceDto = new MemberServiceDto(email, nickname);
 
@@ -64,6 +67,18 @@ public class MemberService {
         if (!memberRepository.existsById(id)) {
             throw new MemberNotFoundException();
         }
+    }
+
+    // 중복되지 않는 유니크한 닉네임을 생성하는 메서드
+    private String generateUniqueNickname() {
+        String nickname;
+
+        // 중복되지 않는 닉네임이 나올 때까지 반복
+        do {
+            nickname = randonNinkname.generateRandomNickname();
+        } while (memberRepository.existsByNickname(nickname)); // 중복된 닉네임이 있으면 다시 생성
+
+        return nickname;
     }
 
 }
