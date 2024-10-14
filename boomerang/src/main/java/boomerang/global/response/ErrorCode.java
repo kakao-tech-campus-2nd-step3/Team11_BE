16207c9e189731152c0f95268069898d35b131e6
J-1ac <tkdwns26@naver.com>
2024-10-15 01:56:06 +0900

    refactor: 에러 코드 통일화

--- boomerang/src/main/java/boomerang/global/response/ErrorCode.java
+++ boomerang/src/main/java/boomerang/global/response/ErrorCode.java
@@ -14,8 +14,10 @@ public enum ErrorCode {
     DUPLICATE_NICKNAME_ERROR(HttpStatus.BAD_REQUEST, "EM003", "Duplicate Nickname Error"),
 
     // Like
-    DUPLICATE_LIKE_ERROR(HttpStatus.BAD_REQUEST, "EL001", "Duplicate Like Error"),
-    NOT_MEMBERS_LIKE_ERROR(HttpStatus.FORBIDDEN, "EL002", "Not Members Like Error"),
+    DUPLICATE_LIKE_ERROR(HttpStatus.BAD_REQUEST, "LK001", "Duplicate Like Error"),
+    NOT_MEMBERS_LIKE_ERROR(HttpStatus.FORBIDDEN, "LK002", "Not Members Like Error"),
+    LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT,"LK003" ,"이미 좋아요한 게시물입니다." ),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "LK004", "해당 좋아요를 찾을 수 없습니다."),
 
     BOARD_NOT_FOUND_ERROR(HttpStatus.BAD_REQUEST, "EB001", "Board Not Found Error"),
     BOARD_DONT_HAS_OWNERSHIP_ERROR(HttpStatus.BAD_REQUEST, "EB002", "수정 또는 삭제 권한이 없는 게시글입니다"),
@@ -32,9 +34,6 @@ public enum ErrorCode {
     LOGIN_REQUIRED(HttpStatus.BAD_REQUEST,"MB_002" ,"로그인이 필요합니다." ),
 
 
-    //Like
-    LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT,"LK_001" ,"이미 좋아요한 게시물입니다." ),
-    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "LK_002", "해당 좋아요를 찾을 수 없습니다."),
 
 
 
