3b15cb650c8d6557470fbee4fb55884ca05b3325
JaeBin <201924576@pusan.ac.kr>
2024-10-14 19:16:02 +0900

    feat: mentor entity 생성

---
+++ boomerang/src/main/java/boomerang/mentor/domain/Mentor.java
@@ -0,0 +1,70 @@
+package boomerang.mentor.domain;
+
+import boomerang.IsDeleted;
+import boomerang.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Entity
+@Table(name = "mentor")
+public class Mentor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MentorType mentoType;
+
+    private String career;
+
+    private String introduce;
+
+    private Boolean displayStatus;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private IsDeleted isDeleted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    protected Mentor() {
+    }
+
+    public Mentor(MentorType mentoType, String career, String introduce, Boolean displayStatus, Member member) {
+        this.mentoType = mentoType;
+        this.career = career;
+        this.introduce = introduce;
+        this.displayStatus = displayStatus;
+        this.member = member;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        Mentor mentor = (Mentor) o;
+        return Objects.equals(id, mentor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
