fba6993a062282da8130da09ca189249081f7919
JinSeoHyun <15roa1121@gmail.com>
2024-10-15 21:30:12 +0900

    feat : 진행도 내부의 메인 단계 구현

---
+++ boomerang/src/main/java/boomerang/progress/domain/MainStepEx.java
@@ -0,0 +1,41 @@
+package boomerang.progress.domain;
+
+import boomerang.progress.dto.SubStepResponseDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Embeddable
+public class MainStepEx implements MainStep{
+
+    private Boolean subStep1;
+
+    private Boolean subStep2;
+
+    public MainStepEx() {
+        this.subStep1 = false;
+        this.subStep2 = false;
+    }
+
+    @Override
+    public String getMainName() {
+        return "진행도1번";
+    }
+
+    @Override
+    public List<SubStepResponseDto> getSubStepAllForResponse() {
+        List<SubStepResponseDto> response = new ArrayList<>();
+        response.add(new SubStepResponseDto("sub_step1", subStep1));
+        response.add(new SubStepResponseDto("sub_step2", subStep2));
+        return response;
+    }
+
+    @Override
+    public Boolean getCompletion() {
+        return this.subStep1 && this.subStep2;
+    }
+}
