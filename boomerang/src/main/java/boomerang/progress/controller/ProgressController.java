86836a66397abfd7be1789608eb678e7cc002cd5
JinSeoHyun <15roa1121@gmail.com>
2024-10-15 21:09:00 +0900

    feat(ProgressController.java) : 진행도 조회 API 구현 1. 진행도 전체 조회 2. 메인 단계의 진행도 조회 3. 세부 단계의 진행 조회

--- boomerang/src/main/java/boomerang/progress/controller/ProgressController.java
+++ boomerang/src/main/java/boomerang/progress/controller/ProgressController.java
@@ -1,8 +1,13 @@
 package boomerang.progress.controller;
 
 import boomerang.global.oauth.dto.PrincipalDetails;
+import boomerang.progress.domain.MainStepEnum;
 import boomerang.progress.domain.ProgressType;
+import boomerang.progress.domain.SubStepEnum;
+import boomerang.progress.dto.MainStepResponseDto;
+import boomerang.progress.dto.ProgressDetailsResponseDto;
 import boomerang.progress.dto.ProgressTypeRequestDto;
+import boomerang.progress.dto.SubStepResponseDto;
 import boomerang.progress.service.ProgressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +22,7 @@ public class ProgressController {
 
     private final ProgressService progressService;
 
-    @PostMapping("/progress/type-check")
+    @PostMapping("/progress/type")
     public ResponseEntity<?> checkUserType(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                            @RequestBody ProgressTypeRequestDto progressTypeRequestDto) {
         ProgressType progressTypeOfMember = progressService.checkUserType(principalDetails, progressTypeRequestDto);
@@ -25,9 +30,42 @@ public class ProgressController {
     }
 
     @GetMapping("/progress/type")
-    public ResponseEntity<?> checkUserType(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public ResponseEntity<?> getUserType(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         ProgressType progressTypeOfMember = progressService.getUserType(principalDetails);
         return ResponseEntity.status(HttpStatus.OK).body(progressTypeOfMember);
+    }
+
+    //유저의 메인 단계 목록 조회
+    @GetMapping("/progress")
+    public ResponseEntity<?> getProgressDetails(@AuthenticationPrincipal PrincipalDetails principalDetails){
+        ProgressDetailsResponseDto progressDetailsResponseDto = progressService.getProgressDetails(principalDetails);
+        return ResponseEntity.status(HttpStatus.OK).body(progressDetailsResponseDto);
+    }
+
+    //유저의 특정 메인 단계의 서브 단계 목록 조회
+    @GetMapping("/progress/{main}")
+    public ResponseEntity<?> getSubStepsByMainStep(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                              @PathVariable("main") MainStepEnum mainStepEnum){
+        MainStepResponseDto mainStepResponseDto = progressService.getSubStepsByMainStep(principalDetails, mainStepEnum);
+        return ResponseEntity.status(HttpStatus.OK).body(mainStepResponseDto);
+    }
+
+    //유저의 메인 단계의 서브 단계 목록 조회
+    @GetMapping("/progress/{main}/{sub}")
+    public ResponseEntity<?> getSubStepStatus(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                              @PathVariable("main") MainStepEnum mainStepEnum,
+                                              @PathVariable("sub") SubStepEnum subStepEnum){
+        SubStepResponseDto subStepResponseDto = progressService.getSubStepStatus(principalDetails, mainStepEnum, subStepEnum);
+        return ResponseEntity.status(HttpStatus.OK).body(subStepResponseDto);
+    }
+
+    @PostMapping("/progress/{main}/{sub}")
+    public ResponseEntity<?> completeProgress(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                              @PathVariable("main") MainStepEnum mainStepEnum,
+                                              @PathVariable("sub") SubStepEnum subStepEnum) {
+
+        SubStepResponseDto subStepResponseDto = progressService.completeProgress(principalDetails, mainStepEnum, subStepEnum);
+        return ResponseEntity.status(HttpStatus.OK).body(subStepResponseDto);
     }
 
 
--- boomerang/src/main/java/boomerang/progress/domain/MainStep.java
+++ boomerang/src/main/java/boomerang/progress/domain/MainStep.java
@@ -1,34 +1,11 @@
 package boomerang.progress.domain;
 
-import boomerang.global.exception.BusinessException;
-import boomerang.global.response.ErrorCode;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import boomerang.progress.dto.SubStepResponseDto;
 
-public enum MainStep {
-    MAIN_STEP_1( "mainStep1"),
-    ;
+import java.util.List;
 
-    private final String mainStepName;
-
-    MainStep(String mainStepName) {
-        this.mainStepName = mainStepName;
-    }
-
-    @JsonValue
-    public String getSubStepName() {
-        return mainStepName;
-    }
-
-    @JsonCreator
-    public static MainStep fromStepName(String stepName) {
-        for (MainStep mainStep : MainStep.values()) {
-            if (mainStep.mainStepName.equals(stepName)) {
-                return mainStep;
-            }
-        }
-        throw new BusinessException(ErrorCode.PROGRESS_MAIN_INVALID_NAME);
-    }
-
-
+public interface MainStep {
+    String getMainName();
+    List<SubStepResponseDto> getSubStepAllForResponse();
+    Boolean getCompletion();
 }
