44445c8d43e007e3d6734e92f74b907579c75d94
JinSeoHyun <15roa1121@gmail.com>
2024-10-15 16:35:55 +0900

    feat : 진행도 업데이트와 타입 검사시 진행도 엔티티가 생성될 수 있도록 수정

--- boomerang/src/main/java/boomerang/progress/service/ProgressService.java
+++ boomerang/src/main/java/boomerang/progress/service/ProgressService.java
@@ -5,8 +5,14 @@ import boomerang.global.oauth.dto.PrincipalDetails;
 import boomerang.global.response.ErrorCode;
 import boomerang.member.domain.Member;
 import boomerang.member.repository.MemberRepository;
+import boomerang.progress.domain.MainStep;
+import boomerang.progress.domain.Progress;
 import boomerang.progress.domain.ProgressType;
+import boomerang.progress.domain.SubStep;
 import boomerang.progress.dto.ProgressTypeRequestDto;
+import boomerang.progress.dto.SubStepResponseDto;
+import boomerang.progress.repository.ProgressRepository;
+import boomerang.progress.util.ProgressStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +21,10 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ProgressService {
+
     private final MemberRepository memberRepository;
+    private final ProgressRepository progressRepository;
+
 
     public ProgressType checkUserType(PrincipalDetails principalDetails, ProgressTypeRequestDto progressTypeRequestDto) {
         Member member = getMemberOrThrow(principalDetails.getMemberEmail());
@@ -25,13 +34,38 @@ public class ProgressService {
         }
 
         ProgressType progressType = checkType(progressTypeRequestDto.getIsInsured(), progressTypeRequestDto.getIsContractTerminated());
+        Progress progress = progressRepository.save(new Progress(member));
 
         //멤버 엔티티에 피해 유형 등록
         member.registerProgressType(progressType);
 
+        //멤버 엔티티에 진행도 저장
+        member.registerProgress(progress);
+
         memberRepository.save(member);
+
         return progressType;
     }
+
+    //진행도 업데이트
+    public SubStepResponseDto completeProgress(PrincipalDetails principalDetails, MainStep mainStep, SubStep subStep) {
+
+        if (!subStep.isMatchingMainStep(mainStep)) {
+            throw new BusinessException(ErrorCode.PROGRESS_SUB_MAIN_DO_NOT_MATCH);
+        }
+
+        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Progress progress = Optional.ofNullable(member.getProgress())
+                .orElseThrow(() -> new BusinessException(ErrorCode.PROGRESS_TYPE_NON_EXISTENT));
+        //타입검사도 필요함.. ㅜㅜ
+
+        Progress updatedProgress = ProgressStrategy.updateProgress(progress, mainStep, subStep);
+        progressRepository.save(updatedProgress);
+
+        return new SubStepResponseDto(updatedProgress);
+
+    }
+
 
     public ProgressType getUserType(PrincipalDetails principalDetails) {
         Member member = getMemberOrThrow(principalDetails.getMemberEmail());
@@ -43,6 +77,7 @@ public class ProgressService {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NON_EXISTENT));
     }
+
 
     private ProgressType checkType(Boolean isMemberInsureds, Boolean isMemberContractTerminated) {
         if (isMemberInsureds && isMemberContractTerminated) {
