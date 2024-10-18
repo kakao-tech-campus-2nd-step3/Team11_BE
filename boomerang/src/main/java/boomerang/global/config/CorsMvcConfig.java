29d8987f1935f29c351b4d75171cfffbd0b4710f
Dalsungmin <cnww123@naver.com>
2024-10-18 20:05:23 +0900

    프론트 포트 변경

--- boomerang/src/main/java/boomerang/global/config/CorsMvcConfig.java
+++ boomerang/src/main/java/boomerang/global/config/CorsMvcConfig.java
@@ -12,6 +12,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
         //mvc에서 cors 해결
         corsRegistry.addMapping("/**")
             .exposedHeaders("Set-Cookie")
-            .allowedOrigins("http://localhost:3000");
+            .allowedOrigins("http://localhost:5173");
     }
 }
