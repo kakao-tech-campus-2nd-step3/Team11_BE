783981b749d99a9b463a5aee699113415959d134
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 21:46:04 +0900

    feat(ConsultationRequestDto) : 유효성 로직 추가

--- boomerang/src/main/java/boomerang/consultation/dto/ConsultationRequestDto.java
+++ boomerang/src/main/java/boomerang/consultation/dto/ConsultationRequestDto.java
@@ -3,6 +3,7 @@ package boomerang.consultation.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ConsultationRequestDto {
+    @Positive(message = "Mentor ID는 빈값일 수 없습니다.")
     private long mentorId;
 }
