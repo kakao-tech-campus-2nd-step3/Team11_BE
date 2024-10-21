793c90161e8e23453900c90d2a57b868b89623e6
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 18:35:55 +0900

    feat(ConsultationRequestDto) : 상담요청객체 추가

---
+++ boomerang/src/main/java/boomerang/consultation/dto/ConsultationRequestDto.java
@@ -0,0 +1,15 @@
+package boomerang.consultation.dto;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ConsultationRequestDto {
+    private long mentorId;
+}
