5112f39dce94c757b6359ee1eb68ed2e2848516e
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 21:47:57 +0900

    feat: 동일한 날짜 & 멘티 & 멘토 존재검증 메서드 추가

--- boomerang/src/main/java/boomerang/consultation/repository/ConsultationRepository.java
+++ boomerang/src/main/java/boomerang/consultation/repository/ConsultationRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
 
     Page<Consultation> findAllByMentee(Member mentee, Pageable pageable);
 
     Page<Consultation> findAllByMentor(Mentor mentor, Pageable pageable);
+
+    boolean existsByMenteeAndMentorAndConsultationDate(Member mentee, Mentor mentor, LocalDate consultationDate);
 }
