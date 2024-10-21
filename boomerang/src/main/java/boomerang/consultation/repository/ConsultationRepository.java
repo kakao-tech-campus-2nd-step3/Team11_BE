6188800619e81b1a3ae71a7944bc0d5288220a78
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 21:21:52 +0900

    feat(ConsultationRepository) : 멘토,멘티별 상담내역 페이지 조회 메서드 구현

--- boomerang/src/main/java/boomerang/consultation/repository/ConsultationRepository.java
+++ boomerang/src/main/java/boomerang/consultation/repository/ConsultationRepository.java
@@ -1,7 +1,15 @@
 package boomerang.consultation.repository;
 
 import boomerang.consultation.domain.Consultation;
+import boomerang.member.domain.Member;
+import boomerang.mentor.domain.Mentor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+
+    Page<Consultation> findAllByMentee(Member mentee, Pageable pageable);
+
+    Page<Consultation> findAllByMentor(Mentor mentor, Pageable pageable);
 }
