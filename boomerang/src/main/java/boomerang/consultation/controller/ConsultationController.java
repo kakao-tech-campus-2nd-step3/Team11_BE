798f964c9030e705177f88f6c5fa93ee3592b79c
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 21:21:18 +0900

    feat(ConsultationController) : 상담 관련 api구현 1. id로 상담조회 2. 멘토별 상담 조회 3. 개인별 상담 내역조회

--- boomerang/src/main/java/boomerang/consultation/controller/ConsultationController.java
+++ boomerang/src/main/java/boomerang/consultation/controller/ConsultationController.java
@@ -2,10 +2,11 @@ package boomerang.consultation.controller;
 
 import boomerang.consultation.dto.ConsultationRequestDto;
 import boomerang.consultation.dto.ConsultationResponseDto;
+import boomerang.consultation.dto.ConsultationResponseListDto;
 import boomerang.consultation.service.ConsultationService;
 import boomerang.global.oauth.dto.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,11 +19,11 @@ public class ConsultationController {
 
     private final ConsultationService consultationService;
 
-    //상담등록
+    //상담신청
     @PostMapping("/consultation")
     public ResponseEntity<ConsultationResponseDto> requestConsultation(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                                                        @RequestBody ConsultationRequestDto consultationRequestDto) {
-        ConsultationResponseDto consultationResponseDto = consultationService.requestConsultation(principalDetails,consultationRequestDto);
+        ConsultationResponseDto consultationResponseDto = consultationService.requestConsultation(principalDetails, consultationRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(consultationResponseDto);
     }
 
@@ -31,15 +32,29 @@ public class ConsultationController {
     public ResponseEntity<ConsultationResponseDto> requestConsultation(@AuthenticationPrincipal PrincipalDetails principalDetails,
                                                                        @PathVariable("consultation_id") Long consultationId) {
         ConsultationResponseDto consultationResponseDto = consultationService.completeConsultation(principalDetails, consultationId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(consultationResponseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(consultationResponseDto);
     }
 
+    //상담조회
+    @GetMapping("/consultation/{consultation_id}")
+    public ResponseEntity<ConsultationResponseDto> requestConsultation(@PathVariable("consultation_id") Long consultationId) {
+        ConsultationResponseDto consultationResponseDto = consultationService.getConsultationDetail(consultationId);
+        return ResponseEntity.status(HttpStatus.OK).body(consultationResponseDto);
+    }
+
+
     //멘토별 상담 조회
+    @GetMapping("mentor/{mentor_id}/consultation")
+    public ResponseEntity<ConsultationResponseListDto> getConsultationOfMentor(@PathVariable("mentor_id") Long mentorId, Pageable pageable) {
+        ConsultationResponseListDto consultation = consultationService.getConsultationOfMentor(mentorId, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(consultation);
+    }
 
     //개인별 상담 내역조회
     @GetMapping("user/consultation")
-    public ResponseEntity<Page<ConsultationResponseDto>> getConsultationByUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        return null;
+    public ResponseEntity<ConsultationResponseListDto> getConsultationOfUser(@AuthenticationPrincipal PrincipalDetails principalDetails, Pageable pageable) {
+        ConsultationResponseListDto consultation = consultationService.getConsultationOfUser(principalDetails, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(consultation);
     }
 
 }
