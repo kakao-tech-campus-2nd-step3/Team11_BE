0f015a131d2ed3bf9eea833acbf70bb935b9f5ff
JinSeoHyun <15roa1121@gmail.com>
2024-10-21 21:45:39 +0900

    feat: 이미 해당 날짜&멘토&멘티 상담이 존재하는 경우 에러 처리

--- boomerang/src/main/java/boomerang/consultation/service/ConsultationService.java
+++ boomerang/src/main/java/boomerang/consultation/service/ConsultationService.java
@@ -3,6 +3,7 @@ package boomerang.consultation.service;
 import boomerang.consultation.domain.Consultation;
 import boomerang.consultation.dto.ConsultationRequestDto;
 import boomerang.consultation.dto.ConsultationResponseDto;
+import boomerang.consultation.dto.ConsultationResponseListDto;
 import boomerang.consultation.repository.ConsultationRepository;
 import boomerang.global.exception.BusinessException;
 import boomerang.global.oauth.dto.PrincipalDetails;
@@ -10,8 +11,10 @@ import boomerang.global.response.ErrorCode;
 import boomerang.member.domain.Member;
 import boomerang.member.service.MemberService;
 import boomerang.mentor.domain.Mentor;
-import boomerang.mentor.domain.repository.MentorRepository;
+import boomerang.mentor.service.MentorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -22,11 +25,17 @@ public class ConsultationService {
 
     private final ConsultationRepository consultationRepository;
     private final MemberService memberService;
+    private final MentorService mentorService;
 
     public ConsultationResponseDto requestConsultation(PrincipalDetails principalDetails, ConsultationRequestDto consultationRequestDto) {
         Member mentee = memberService.getMemberByEmail(principalDetails.getMemberEmail());
-//        Mentor mentor = mentorRepository.findById(1L).orElseThrow();
+        Mentor mentor = mentorService.getMentor(consultationRequestDto.getMentorId());
         LocalDate localDate = LocalDate.now();
+
+        if (consultationRepository.existsByMenteeAndMentorAndConsultationDate(mentee, mentor, localDate)) {
+            throw new BusinessException(ErrorCode.CONSULTATION_ALREADY_EXISTS);
+        }
+
 
         Consultation savedConsultation = consultationRepository.save(new Consultation(mentee, mentor, localDate));
 
@@ -47,21 +56,29 @@ public class ConsultationService {
         return new ConsultationResponseDto(savedConsultation);
     }
 
-//    public ConsultationResponseDto getConsultationOfMentor (PrincipalDetails principalDetails,) {
-//        Member mentee = memberService.getMemberByEmail(principalDetails.ge tMemberEmail());
-//        Consultation consultation = getConsultation(consultationId);
-//
-//        if (!consultation.isMentee(mentee)) {
-//            throw new BusinessException(ErrorCode.CONSULTATION_NOT_A_MENTEE);
-//        }
-//
-//        consultation.complete();
-//
-//        Consultation savedConsultation = consultationRepository.save(consultation);
-//        return new ConsultationResponseDto(savedConsultation);
-//    }
+    public ConsultationResponseDto getConsultationDetail(Long consultationId) {
+        Consultation consultation = getConsultation(consultationId);
+        return new ConsultationResponseDto(consultation);
+    }
 
 
+    public ConsultationResponseListDto getConsultationOfUser(PrincipalDetails principalDetails, Pageable pageable) {
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
+
+        Page<ConsultationResponseDto> consultationResponsePage = consultationRepository.findAllByMentee(member, pageable)
+                .map(ConsultationResponseDto::new);
+
+        return new ConsultationResponseListDto(consultationResponsePage);
+    }
+
+    public ConsultationResponseListDto getConsultationOfMentor(Long mentorId, Pageable pageable) {
+        Mentor mentor = mentorService.getMentor(mentorId);
+        Page<ConsultationResponseDto> consultationResponsePage = consultationRepository.findAllByMentor(mentor, pageable)
+                .map(ConsultationResponseDto::new);
+
+        return new ConsultationResponseListDto(consultationResponsePage);
+
+    }
 
     public Consultation getConsultation(long id) {
         return consultationRepository.findById(id)
