78f9fe6f93c66001ce7be3f0543bb3dfb7e3009c
J-1ac <tkdwns26@naver.com>
2024-10-22 23:18:24 +0900

    refactor: JsonNaming SnakeCase 적용

--- boomerang/src/main/java/boomerang/mentor/dto/MentorCreateRequestDto.java
+++ boomerang/src/main/java/boomerang/mentor/dto/MentorCreateRequestDto.java
@@ -1,12 +1,15 @@
 package boomerang.mentor.dto;
 
 import boomerang.mentor.domain.MentorType;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MentorCreateRequestDto {
 
     @NotNull(message = "멘토 유형은 필수입니다.")
