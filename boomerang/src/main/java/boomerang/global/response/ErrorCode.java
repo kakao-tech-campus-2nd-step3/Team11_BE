75ef45a5e30171a01f92b0505b69cc656d2c3ee8
JinSeoHyun <15roa1121@gmail.com>
2024-10-25 19:23:03 +0900

    fix : 에러 설명이 다르게 매칭된 부분 수정

--- boomerang/src/main/java/boomerang/global/response/ErrorCode.java
+++ boomerang/src/main/java/boomerang/global/response/ErrorCode.java
@@ -46,8 +46,8 @@ public enum ErrorCode {
     PROGRESS_SUB_INVALID_NAME(HttpStatus.BAD_REQUEST, "PG006", "세부 단계의 이름이 잘못되었습니다."),
     PROGRESS_MAIN_INVALID_NAME(HttpStatus.BAD_REQUEST, "PG007", "메인 단계의 이름이 잘못되었습니다."),
     PROGRESS_SUB_MAIN_DO_NOT_MATCH(HttpStatus.BAD_REQUEST, "PG008", "서브단계와 메인 단계가 적절하게 매칭되지 않습니다."),
-    PROGRESS_SUB_ERROR(HttpStatus.NOT_FOUND, "PG009", "해당 메인단계에 대한 정보가 없습니다."),
-    PROGRESS_MAIN_ERROR(HttpStatus.NOT_FOUND, "PG010", "해당 세부단계에 대한 정보가 없습니다."),
+    PROGRESS_SUB_ERROR(HttpStatus.NOT_FOUND, "PG009", "해당 세부단계에 대한 정보가 없습니다."),
+    PROGRESS_MAIN_ERROR(HttpStatus.NOT_FOUND, "PG010", "해당 메인단계에 대한 정보가 없습니다."),
 
     ;
 
