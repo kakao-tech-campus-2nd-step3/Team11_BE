4bf8513f258254fcfc1b15ba377a216bd8037a41
JinSeoHyun <15roa1121@gmail.com>
2024-10-27 21:34:33 +0900

    refactor: 진행도,메인단계,서브단계 응답 객체 수정

--- boomerang/src/main/java/boomerang/progress/dto/ProgressDetailsResponseDto.java
+++ boomerang/src/main/java/boomerang/progress/dto/ProgressDetailsResponseDto.java
@@ -1,9 +1,7 @@
 package boomerang.progress.dto;
 
-import boomerang.progress.domain.MainStep;
 import boomerang.progress.domain.Progress;
 import boomerang.progress.domain.ProgressType;
-import boomerang.progress.util.ProgressStrategy;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
@@ -16,9 +14,9 @@ public class ProgressDetailsResponseDto  {
     private ProgressType progressType; //유저의 타입
     private List<MainStepResponseDto> mainStepList;
 
-    public ProgressDetailsResponseDto(Progress progress, List<MainStep> mainStepList) {
+    public ProgressDetailsResponseDto(Progress progress) {
         this.progressType = progress.getProgressType();
-        this.mainStepList = mainStepList
+        this.mainStepList = progress.getMainStepList()
                 .stream()
                 .map(MainStepResponseDto::new)
                 .toList();
