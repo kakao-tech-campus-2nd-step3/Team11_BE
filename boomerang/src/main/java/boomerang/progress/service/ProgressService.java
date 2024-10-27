ab90699a0fa7db3bd352727c6bbadcb6eacf747d
JinSeoHyun <15roa1121@gmail.com>
2024-10-27 21:36:21 +0900

    refactor: 전략 패턴 수정과 로직 리팩토링

--- boomerang/src/main/java/boomerang/progress/service/ProgressService.java
+++ boomerang/src/main/java/boomerang/progress/service/ProgressService.java
@@ -4,153 +4,135 @@ import boomerang.global.exception.BusinessException;
 import boomerang.global.oauth.dto.PrincipalDetails;
 import boomerang.global.response.ErrorCode;
 import boomerang.member.domain.Member;
-import boomerang.member.repository.MemberRepository;
+import boomerang.member.service.MemberService;
 import boomerang.progress.domain.*;
-import boomerang.progress.dto.*;
+import boomerang.progress.dto.MainStepResponseDto;
+import boomerang.progress.dto.ProgressDetailsResponseDto;
+import boomerang.progress.dto.ProgressTypeRequestDto;
+import boomerang.progress.dto.SubStepResponseDto;
 import boomerang.progress.repository.ProgressRepository;
 import boomerang.progress.util.ProgressStrategy;
+import boomerang.progress.util.ProgressStrategyFactory;
+import boomerang.progress.util.ProgressTypeResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 public class ProgressService {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final ProgressRepository progressRepository;
 
-
     //유저의 타입 검사
+    @Transactional
     public ProgressType checkUserType(PrincipalDetails principalDetails, ProgressTypeRequestDto progressTypeRequestDto) {
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
 
         if (member.hasProgress()) {
             throw new BusinessException(ErrorCode.PROGRESS_TYPE_EXISTS);
         }
 
-        ProgressType progressType = checkType(progressTypeRequestDto); //타입계산
+        ProgressType progressType = ProgressTypeResolver.checkType(progressTypeRequestDto);
 
-        Progress savedProgress = progressRepository.save(new Progress(member, progressType));
+        ProgressStrategy progressStrategy = ProgressStrategyFactory.getStrategy(progressType);
+        Progress savedProgress = progressRepository.save(progressStrategy.makeProgress(member));
         member.registerProgress(savedProgress);
-        memberRepository.save(member);
 
         return progressType;
     }
 
     //유저의 타입 조회
     public ProgressType getUserType(PrincipalDetails principalDetails) {
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
-        Progress progress = getProgressByMember(member);
-
-        return progress.getProgressType();
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
+        return Optional.ofNullable(member.getProgressType())
+                .orElseThrow(() -> new BusinessException(ErrorCode.PROGRESS_TYPE_NON_EXISTENT));
     }
 
     //유저의 진행도 전체 조회
+    @Transactional(readOnly = true)
     public ProgressDetailsResponseDto getProgressDetails(PrincipalDetails principalDetails) {
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
         Progress progress = getProgressByMember(member);
 
-        List<MainStep> mainStepList = ProgressStrategy.getActiveMainStepList(progress);
-
-        return new ProgressDetailsResponseDto(progress,mainStepList);
+        return new ProgressDetailsResponseDto(progress);
     }
 
     //특정 메인 단계만 조회
+    @Transactional(readOnly = true)
     public MainStepResponseDto getSubStepsByMainStep(PrincipalDetails principalDetails, MainStepEnum mainStepEnum) {
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        System.out.println("mainStepEnum = " + mainStepEnum.getMainStepName());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
         Progress progress = getProgressByMember(member);
-        MainStep mainStep = ProgressStrategy.getMainStep(progress,mainStepEnum);
+
+        MainStep mainStep = getMainStepByEnum(progress, mainStepEnum);
 
         return new MainStepResponseDto(mainStep);
     }
 
     //특정 서브단계만 조회
+    @Transactional(readOnly = true)
     public SubStepResponseDto getSubStepStatus(PrincipalDetails principalDetails,
                                                MainStepEnum mainStepEnum,
                                                SubStepEnum subStepEnum) {
-        //메인단계와 서브단계가 적절한 매칭인지 검사
-        if (!subStepEnum.isMatchingMainStep(mainStepEnum)) {
-            throw new BusinessException(ErrorCode.PROGRESS_SUB_MAIN_DO_NOT_MATCH);
-        }
-
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
         Progress progress = getProgressByMember(member);
 
-        SubStepDto subStepDto = ProgressStrategy.getSubStep(progress,subStepEnum);
+        MainStep mainStep = getMainStepByEnum(progress, mainStepEnum);
+        SubStep subStep = getSubStepByEnum(mainStep, subStepEnum);
 
-        //전략객체
-        return new SubStepResponseDto(subStepDto);
+        return new SubStepResponseDto(subStep);
     }
 
     //진행도 업데이트
+    @Transactional
     public SubStepResponseDto completeProgress(PrincipalDetails principalDetails, MainStepEnum mainStepEnum, SubStepEnum subStepEnum) {
-        //메인스탭이랑 서브 스텝이 매칭이 되는지를 검사
-        if (!subStepEnum.isMatchingMainStep(mainStepEnum)) {
-            throw new BusinessException(ErrorCode.PROGRESS_SUB_MAIN_DO_NOT_MATCH);
-        }
 
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
         Progress progress = getProgressByMember(member);
 
-        SubStepDto subStepDto = ProgressStrategy.completeProgress(progress, subStepEnum);
-        progressRepository.save(progress);
+        MainStep mainStep = getMainStepByEnum(progress, mainStepEnum);
+        SubStep subStep = getSubStepByEnum(mainStep, subStepEnum);
 
-        return new SubStepResponseDto(subStepDto);
-
-
+        subStep.markAsComplete();
+        return new SubStepResponseDto(subStep);
     }
 
     //진행도 완료 취소
+    @Transactional
     public SubStepResponseDto revertProgressToIncomplete(PrincipalDetails principalDetails, MainStepEnum mainStepEnum, SubStepEnum subStepEnum) {
-        //메인스탭이랑 서브 스텝이 매칭이 되는지를 검사
-        if (!subStepEnum.isMatchingMainStep(mainStepEnum)) {
-            throw new BusinessException(ErrorCode.PROGRESS_SUB_MAIN_DO_NOT_MATCH);
-        }
-
-        Member member = getMemberOrThrow(principalDetails.getMemberEmail());
+        Member member = memberService.getMemberByEmail(principalDetails.getMemberEmail());
         Progress progress = getProgressByMember(member);
 
-        SubStepDto subStepDto = ProgressStrategy.revertProgressToIncomplete(progress, subStepEnum);
-        progressRepository.save(progress);
-        return new SubStepResponseDto(subStepDto);
+        MainStep mainStep = getMainStepByEnum(progress, mainStepEnum);
+        SubStep subStep = getSubStepByEnum(mainStep, subStepEnum);
+
+        subStep.markAsIncomplete();
+        return new SubStepResponseDto(subStep);
 
     }
 
 
+    private MainStep getMainStepByEnum(Progress progress, MainStepEnum mainStepEnum) {
+        return progress.getMainStepByEnum(mainStepEnum)
+                .orElseThrow(() -> new BusinessException(ErrorCode.PROGRESS_NOT_INCLUDED_MAIN));
+    }
+
+    private SubStep getSubStepByEnum(MainStep mainStep, SubStepEnum subStepEnum) {
+        return mainStep.getSubStepByEnum(subStepEnum)
+                .orElseThrow(() -> new BusinessException(ErrorCode.PROGRESS_NOT_INCLUDED_SUB));
+    }
 
     private Progress getProgressByMember(Member member) {
         Progress progress = member.getProgress();
-
         if (progress == null) {
-            throw new BusinessException(ErrorCode.PROGRESS_TYPE_EXISTS);
+            throw new BusinessException(ErrorCode.PROGRESS_NON_EXISTENT);
         }
         return progress;
-    }
-
-    private Member getMemberOrThrow(String email) {
-        return memberRepository.findByEmail(email).orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NON_EXISTENT));
-    }
-
-    private ProgressType checkType(ProgressTypeRequestDto progressTypeRequestDto) {
-        Boolean isMemberInsureds = progressTypeRequestDto.getIsInsured();
-        Boolean isMemberContractTerminated = progressTypeRequestDto.getIsContractTerminated();
-
-        if (isMemberInsureds && isMemberContractTerminated) {
-            return ProgressType.D;
-        }
-        if (!isMemberInsureds && isMemberContractTerminated) {
-            return ProgressType.C;
-        }
-        if (isMemberInsureds && !isMemberContractTerminated) {
-            return ProgressType.B;
-        }
-        if (!isMemberInsureds && !isMemberContractTerminated) {
-            return ProgressType.A;
-        }
-        throw new BusinessException(ErrorCode.PROGRESS_TYPE_REQUEST_ERROR);
     }
 
 
