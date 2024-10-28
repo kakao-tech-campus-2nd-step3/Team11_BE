dee8ac5df4bff13a0e8e1b5387318d7c3da7a23c
JinSeoHyun <15roa1121@gmail.com>
2024-10-29 02:19:19 +0900

    fix : 리디렉션 오류 수정

--- boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
+++ boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
@@ -9,11 +9,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -80,9 +81,20 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
+        String path = request.getRequestURI();
+        String method = request.getMethod();
+
+        // 'welcome' 페이지와 /api/v1/member/random-nickname요청은 필터링하지 않음
+        if ("/welcome".equals(path) || ("/api/v1/member/random-nickname".equals(path))) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         //MemberDetails에 회원 정보 객체 담기
         PrincipalDetails memberDetail = (PrincipalDetails) principalService.loadUserByEmail(email);
-        if (memberDetail.getMemberRole() == MemberRole.INCOMPLETE_USER) {
+
+        if (memberDetail.getMemberRole().equals(MemberRole.INCOMPLETE_USER)
+                && !("/api/v1/member/nickname".equals(path) && "PUT".equalsIgnoreCase(method))) {
             response.sendRedirect(clientServerProperties.getWelcome());
             filterChain.doFilter(request, response);
         }
