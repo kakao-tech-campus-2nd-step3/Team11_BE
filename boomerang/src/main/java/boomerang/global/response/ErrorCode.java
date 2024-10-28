0e95e6b002a86c9a97db2d67ee04ae0d612976ba
JinSeoHyun <15roa1121@gmail.com>
2024-10-29 02:11:14 +0900

    fix : 문자열 쿠키로 변환시 공백포함불가 인코딩 과정 추가

--- boomerang/src/main/java/boomerang/global/response/ErrorCode.java
+++ boomerang/src/main/java/boomerang/global/response/ErrorCode.java
@@ -7,6 +7,7 @@ public enum ErrorCode {
     UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EG001", "Unexpected Error"),
     ACCESS_TOKEN_NOT_EXISTS_ERROR(HttpStatus.BAD_REQUEST, "EG002", "Access Token Not Exists Error"),
     JWT_ERROR(HttpStatus.UNAUTHORIZED, "EG003", "JWT token is not valid"),
+    COOKIES_ERROR(HttpStatus.UNAUTHORIZED, "EG004", "닉네임 쿠키 생성중 오류가 발생했습니다."),
 
     // Template
     TEMPLATE_NOT_FOUND_ERROR(HttpStatus.BAD_REQUEST, "EM001", "Template Not Found Error"),
--- boomerang/src/main/java/boomerang/global/utils/CookieUtil.java
+++ boomerang/src/main/java/boomerang/global/utils/CookieUtil.java
@@ -1,6 +1,11 @@
 package boomerang.global.utils;
 
+import boomerang.global.exception.BusinessException;
+import boomerang.global.response.ErrorCode;
 import jakarta.servlet.http.Cookie;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class CookieUtil {
 
@@ -18,13 +23,19 @@ public class CookieUtil {
     }
 
     public static Cookie createNicknameCookies(String value) {
+        try {
+            // 공백과 특수 문자를 인코딩
+            String encodedValue = URLEncoder.encode(value, StandardCharsets.UTF_8);
 
-        Cookie cookie = new Cookie(Nickname, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(false);
-        cookie.setMaxAge(60 * 60 * 60);
+            Cookie cookie = new Cookie("Nickname", encodedValue);
+            cookie.setPath("/");
+            cookie.setHttpOnly(false);
+            cookie.setMaxAge(60 * 60 * 60);
 
-        return cookie;
+            return cookie;
+        } catch (Exception e) {
+            throw new BusinessException(ErrorCode.COOKIES_ERROR);
+        }
     }
 
 }
