65292c95b09d44f21c40eeb30629c9d7ffa53258
JinSeoHyun <15roa1121@gmail.com>
2024-10-28 16:43:39 +0900

    remove: 필요없어진 파일 삭제

--- boomerang/src/main/java/boomerang/progress/util/ProgressStrategy.java
+++
@@ -1,9 +0,0 @@
-package boomerang.progress.util;
-
-import boomerang.member.domain.Member;
-import boomerang.progress.domain.*;
-
-
-public interface ProgressStrategy {
-    Progress makeProgress(Member member);
-}
