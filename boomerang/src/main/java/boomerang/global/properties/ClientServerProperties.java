242121472bd1d4a193d8153ede48cea349ada8b0
JinSeoHyun <15roa1121@gmail.com>
2024-10-29 02:20:19 +0900

    fixClientServerProperties) : 프로퍼티클래스에 setter가 없어서 주입이 안되는 문제 해결

--- boomerang/src/main/java/boomerang/global/properties/ClientServerProperties.java
+++ boomerang/src/main/java/boomerang/global/properties/ClientServerProperties.java
@@ -3,11 +3,15 @@ package boomerang.global.properties;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 @Component
 @ConfigurationProperties(prefix = "client.server")
 public class ClientServerProperties {
+    @Value("${client.server.home}")
     private String home;
+
+    @Value("${client.server.welcome}")
     private String welcome;
 }
