1faa89b7efd71d797e7d8d4756582c930698ec6d
JinSeoHyun <15roa1121@gmail.com>
2024-10-30 16:59:57 +0900

    fix: 쿠키를 도메인 설정

--- boomerang/src/main/java/boomerang/global/utils/CookieUtil.java
+++ boomerang/src/main/java/boomerang/global/utils/CookieUtil.java
@@ -3,6 +3,7 @@ package boomerang.global.utils;
 import boomerang.global.exception.BusinessException;
 import boomerang.global.response.ErrorCode;
 import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 
 import java.net.URLEncoder;
@@ -12,11 +13,14 @@ public class CookieUtil {
 
     public static String Authorization = "Authorization";
     public static String Nickname = "Nickname";
+    @Value("${app.server.ip}")
+    private static String serverIp;
 
     public static ResponseCookie createAuthorizationCookie(String value) {
         return ResponseCookie.from(Authorization, value)
                 .path("/")
                 .httpOnly(true)
+                .domain(serverIp)
                 .secure(true)         // HTTPS 전송을 위한 설정
                 .sameSite("None")     // 크로스 도메인 요청에서도 쿠키 전송
                 .maxAge(60 * 60 * 60) // 쿠키 수명 설정
@@ -31,6 +35,7 @@ public class CookieUtil {
             return ResponseCookie.from(Nickname, encodedValue)
                     .path("/")
                     .httpOnly(false)         // HTTP 전용 아님
+                    .domain(serverIp)
                     .sameSite("None")        // 크로스 도메인 요청에서도 쿠키 전송 가능
                     .maxAge(60 * 60 * 60)    // 쿠키 수명 설정
                     .build();
