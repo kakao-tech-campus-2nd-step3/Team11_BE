1717592a7c31c8bd90f7291c672f407bdaecf0da
JinSeoHyun <15roa1121@gmail.com>
2024-10-30 20:50:58 +0900

    feat: 응답 헤더에서 토큰 가져오기

--- boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
+++ boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
@@ -32,35 +32,12 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         //cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
-        String authorization = null;
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        for (Cookie cookie : cookies) {
-
-            System.out.println(cookie.getName());
-            if (cookie.getName().equals("Authorization")) {
-
-                authorization = cookie.getValue();
-            }
-        }
-
-        //Authorization 헤더 검증
-        if (authorization == null) {
-
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isBlank()) {
             System.out.println("token null");
             filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료 (필수)
             return;
         }
-
-        //토큰
-        String token = authorization;
 
         //토큰 소멸 시간 검증
         if (jwtUtil.isTokenExpired(token)) {
