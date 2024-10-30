7d570fb55c72037fdfa73ab48bffec5f49a9ee30
JinSeoHyun <15roa1121@gmail.com>
2024-10-30 20:19:25 +0900

    fix: 쿠키 설정

--- boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
+++ boomerang/src/main/java/boomerang/global/utils/JwtFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
@@ -52,6 +54,7 @@ public class JwtFilter extends OncePerRequestFilter {
         System.out.println("authorization = " + authorization);
         //Authorization 헤더 검증
 
+        log.debug(authorization.toString());
 
         if (authorization == null) {
 
