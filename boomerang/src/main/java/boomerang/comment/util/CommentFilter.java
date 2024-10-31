898f05e62ce40ffd73b5930ad2348c9079351c58
JinSeoHyun <15roa1121@gmail.com>
2024-11-01 03:17:54 +0900

    feat : 댓글 필터링 클래스 구현

---
+++ boomerang/src/main/java/boomerang/comment/util/CommentFilter.java
@@ -0,0 +1,69 @@
+package boomerang.comment.util;
+
+import boomerang.global.exception.BusinessException;
+import boomerang.global.response.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+@Component
+public class CommentFilter {
+    private BloomFilter<String> profanityFilter;
+    private Pattern profanityPattern;
+
+    //욕설이 저장된 json 파일
+    @Value("${resources.path.bad-words-file}")
+    private String badWordsFilePath;
+
+    @PostConstruct
+    public void init() {
+        ClassPathResource resource = new ClassPathResource(badWordsFilePath);
+        ObjectMapper mapper = new ObjectMapper();
+
+        StringBuilder patternBuilder = new StringBuilder();
+
+        try (InputStream inputStream = resource.getInputStream()) {
+            String[] profanities = mapper.readValue(inputStream, String[].class);
+            this.profanityFilter = BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8),200, 0.01);
+
+            Arrays.sort(profanities, Comparator.comparingInt(String::length).reversed());
+
+            for (String word : profanities) {
+                // 단어 경계를 제거하고 직접 매칭
+                patternBuilder.append(Pattern.quote(word)).append("|");
+                this.profanityFilter.put(Pattern.quote(word));
+            }
+        } catch (IOException e) {
+            throw new BusinessException(ErrorCode.FILE_ERROR);
+        }
+
+        this.profanityPattern = Pattern.compile(patternBuilder.substring(0, patternBuilder.length() - 1), Pattern.CASE_INSENSITIVE);
+    }
+
+    private boolean containsProfanity(String input) {
+        for (String word : input.split("\\s+")) { //공백기준으로 자름
+            if (profanityFilter.mightContain(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String filterProfanity(String text) {
+        if (containsProfanity(text)) {
+            return profanityPattern.matcher(text).replaceAll("***");
+        }
+        return text;
+    }
+}
