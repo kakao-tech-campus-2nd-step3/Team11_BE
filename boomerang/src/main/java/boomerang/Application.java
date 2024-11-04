801ccc150764746f52ce11c2814d2117c29a0e59
J-1ac <tkdwns26@naver.com>
2024-11-05 00:21:28 +0900

    style: 코드 스타일 및 import 구문 정리

--- boomerang/src/main/java/boomerang/Application.java
+++ boomerang/src/main/java/boomerang/Application.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- boomerang/src/main/java/boomerang/email/service/EmailVerificationService.java
+++ boomerang/src/main/java/boomerang/email/service/EmailVerificationService.java
@@ -6,21 +6,14 @@ import boomerang.global.exception.BusinessException;
 import boomerang.global.response.ErrorCode;
 import boomerang.member.domain.Member;
 import boomerang.member.service.MemberService;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @Slf4j
