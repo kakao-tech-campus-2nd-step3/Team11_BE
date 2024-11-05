6dd050cbe86067267adfda184c16a4e42eea0b27
J-1ac <tkdwns26@naver.com>
2024-11-05 22:10:18 +0900

    refactor: SubStepEnum 내용 수정

--- boomerang/src/main/java/boomerang/progress/domain/SubStepEnum.java
+++ boomerang/src/main/java/boomerang/progress/domain/SubStepEnum.java
@@ -7,7 +7,7 @@ import java.util.List;
 import lombok.Getter;
 
 public enum SubStepEnum {
-    SUB_1("주택임차권등기명령 신청서","content", List.of("성명", "주민등록번호", "주소", "연락가능한전화번호", "임대인성명", "임대인주소", "임대차계약일자", "임차보증금액", "주민등록일자", "임차범위", "점유개시일자", "확정일자")),
+    SUB_1("주택임차권등기명령 신청서","content", List.of("성명", "주민등록번호", "주소", "연락가능한전화번호", "임대인성명", "임대인주소", "임대차계약일자", "임차보증금액", "주민등록일자", "임차범위", "점유개시일자", "확정일자", "제출법원명")),
     SUB_2("서브단계2","content", List.of("input1", "input2"));
 
     private final String subStepName;
