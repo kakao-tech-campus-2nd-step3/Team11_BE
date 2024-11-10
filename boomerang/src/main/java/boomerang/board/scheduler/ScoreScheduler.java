33cba43f74fdfa8ca035e3139e3720c2b686c51b
JaeBin <201924576@pusan.ac.kr>
2024-11-11 00:46:10 +0900

    fix: summary 오타 수정

--- boomerang/src/main/java/boomerang/board/scheduler/ScoreScheduler.java
+++ boomerang/src/main/java/boomerang/board/scheduler/ScoreScheduler.java
@@ -4,9 +4,12 @@ import boomerang.board.domain.Board;
 import boomerang.board.repository.BoardRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ScoreScheduler {
 
@@ -30,5 +33,7 @@ public class ScoreScheduler {
 
         // 모든 Board 객체를 한 번에 저장
         boardRepository.saveAll(boards);
+
+        log.info("board score 업데이트 완료");
     }
 }
