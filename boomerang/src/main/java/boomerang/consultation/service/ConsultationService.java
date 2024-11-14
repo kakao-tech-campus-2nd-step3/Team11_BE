d3934a655df47485478043485520b2ac6f7cc3b3
Dalsungmin <150018566+Dalsungmin@users.noreply.github.com>
2024-11-14 13:47:37 +0900

    refactor: 상태 업데이트 로직 서비스에서 스케줄러로 이동

--- boomerang/src/main/java/boomerang/consultation/service/ConsultationService.java
+++ boomerang/src/main/java/boomerang/consultation/service/ConsultationService.java
@@ -1,7 +1,6 @@
 package boomerang.consultation.service;
 
 import boomerang.consultation.domain.Consultation;
-import boomerang.consultation.domain.ConsultationStatus;
 import boomerang.consultation.domain.Schedule;
 import boomerang.consultation.dto.*;
 import boomerang.consultation.repository.ConsultationRepository;
@@ -285,25 +284,5 @@ public class ConsultationService {
                 consultationListRequestDto.getSize(),
                 Sort.by("id").descending()
         );
-    }
-
-    public void updateConsultationStatus(List<Consultation> consultations) {
-
-        // 현재 시간을 가져와 조회된 consultations의 상태를 확인 후 업데이트
-        LocalDateTime now = LocalDateTime.now();
-        consultations.forEach(consultation -> {
-            if (consultation.getConsultationDateTime().isBefore(now)
-                    && consultation.getConsultationStatus() == ConsultationStatus.RECEIVED) {
-                consultation.complete(); // 상태를 FINISHED으로 변경, 상담 시간이 되어도 확정되지 않으면 확정전 -> 상담완료
-            }
-            if (consultation.getConsultationDateTime().isBefore(now)
-                    && consultation.getConsultationStatus() == ConsultationStatus.PENDING) {
-                consultation.start(); // 상태를 ONGOING으로 변경, 확정된 상담이 신청 시간이 지나면 진행전 -> 진행중
-            }
-            if (consultation.getConsultationDateTime().plusHours(1).plusMinutes(30).isBefore(now)
-                    && consultation.getConsultationStatus() == ConsultationStatus.ONGOING) {
-                consultation.complete(); // 상태를 FINISHED으로 변경, 진행중인 상담이 상담시간보다 1시간 30분이 지나면 진행중 -> 상담완료
-            }
-        });
     }
 }
