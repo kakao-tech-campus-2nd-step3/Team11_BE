4e7d09f8794018a2e25d79e775f94698616d6e1c
JinSeoHyun <15roa1121@gmail.com>
2024-11-15 00:34:13 +0900

    fix: 스네이크케이스 적용

--- boomerang/src/main/java/boomerang/member/dto/MemberRoleDto.java
+++ boomerang/src/main/java/boomerang/member/dto/MemberRoleDto.java
@@ -2,9 +2,12 @@ package boomerang.member.dto;
 
 
 import boomerang.member.domain.MemberRole;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MemberRoleDto {
     private MemberRole memberRole;
 
